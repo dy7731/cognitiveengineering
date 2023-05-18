83b84d9241d43a0f1c39cc5d2c0c5b0027888c87
doyeonkim <doxo4080@gmail.com>
2023-05-18 21:08:47 +0900

    project

---
+++ filter/project1.js
@@ -0,0 +1,41 @@
+const products = document.querySelectorAll('.product');
+const radioButtons = document.querySelectorAll('input[name="category"]');
+
+for (const radioButton of radioButtons) {
+  radioButton.addEventListener('change', () => {
+    const category = radioButton.value;
+    for (const product of products) {
+      if (product.dataset.category === category) {
+        product.style.visibility = 'visible';
+        
+        
+       
+      } else {
+        product.style.display = 'none';
+      }
+    }
+  });
+}
+
+
+
+
+function sendCheckedValues() {
+  
+  const checkedValues = [];
+  const checkboxes = document.getElementsByName('check');
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      checkedValues.push(checkbox.value);
+    }
+  });
+
+  // 선택된 값 전달하기
+  const params = new URLSearchParams();
+  params.append('checkedValues', checkedValues.join(';'));
+  const url = 'result.html?' + params.toString();
+  window.open(url, '_blank');
+}
+
+
+
